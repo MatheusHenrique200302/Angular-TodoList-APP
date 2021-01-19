e2d932f92b3730b74f1f790e363c5d4800221e27
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-01-19 20:14:52 -0300

    removed a space

--- ngApp/src/app/login/login.component.ts
+++ ngApp/src/app/login/login.component.ts
@@ -22,7 +22,6 @@ export class LoginComponent implements OnInit {
     this._auth.loginUser(this.loginUserData)
     .subscribe(
       res => {
-      
          localStorage.setItem('token',res.token);
          this._router.navigate(['/special']);
       },
