858c9c6841d091b9414b33252138649d1efd87ec
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-01-19 19:30:09 -0300

    updated env variables

--- ngApp/src/environments/environment.ts
+++ ngApp/src/environments/environment.ts
@@ -3,8 +3,8 @@
 // The list of file replacements can be found in `angular.json`.	
 
 export const environment = {	
-    production: false,
-    MYURL: process.env.MYURL
+    production: true,
+    MYURL: "https://restapi-angulartodoapp.matheushenriq73.repl.co"
   };	
   
   /*	
