6b5e0b0f89e0bb7eb4f58407e36da0c1f99f63f4
MatheusHenrique200302 <36648821+MatheusHenrique200302@users.noreply.github.com>
2021-01-19 16:51:00 -0300

    moved API to Cloud

--- server/models/tasks.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-const tasksSchema = new Schema({
-    user_id: String,
-    taskname: String,
-    taskdesc: String,
-    taskStatus: Boolean
-});
-module.exports = mongoose.model('tasks',tasksSchema, 'tasks')
